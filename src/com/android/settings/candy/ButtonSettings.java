eece2be1dda704f60811b6933dc925965c14284a
rc420head <dennislebhardt@yahoo.com>
2015-12-10 17:22:24 -0600

    we would perfer settings to stick

--- src/com/android/settings/candy/ButtonSettings.java
+++ src/com/android/settings/candy/ButtonSettings.java
@@ -16,20 +16,59 @@
 package com.android.settings.candy;
 
 import android.os.Bundle;
+import android.preference.SwitchPreference;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsLogger;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import java.util.prefs.PreferenceChangeListener;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 
-public class ButtonSettings extends SettingsPreferenceFragment {
-
-    @Override
+public class ButtonSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+	
+	private static final String TAG = ButtonSettings.class.getSimpleName();
+	
+	public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
+	
+	private SwitchPreference mVolumeRockerMusicControl;
+	
+	 @Override
     protected int getMetricsCategory() {
         return MetricsLogger.DEVELOPMENT;
     }
+	
+	 @Override
+     public void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         addPreferencesFromResource(R.xml.candy_settings_buttons);
+ 
+         final ContentResolver resolver = getContentResolver();
+         final PreferenceScreen prefScreen = getPreferenceScreen();
+         final Resources res = getResources();
+    
+        mVolumeRockerMusicControl = (SwitchPreference) findPreference(VOLBTN_MUSIC_CONTROLS);
+        int volumeRockerMusicControl = Settings.System.getInt(getContentResolver(),
+                VOLBTN_MUSIC_CONTROLS, 0);
+        mVolumeRockerMusicControl.setChecked(volumeRockerMusicControl != 0);
+     }
+  
+     @Override
+     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+         return super.onPreferenceTreeClick(preferenceScreen, preference);
+     }
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        addPreferencesFromResource(R.xml.candy_settings_buttons);
-    }
-}
+     public boolean onPreferenceChange(Preference preference, Object newValue) {
+		 if (preference == mVolumeRockerMusicControl) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), VOLBTN_MUSIC_CONTROLS,
+                    value ? 1 : 0);
+            return true;
+         }
+       return false;
+      }  
+  }
--- src/com/android/settings/candy/NavigationBarSettings.java
+++ src/com/android/settings/candy/NavigationBarSettings.java
@@ -17,8 +17,10 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
+    private static final String DOUBLE_TAP_SLEEP_NAVBAR = "double_tap_sleep_navbar";
 
     private SwitchPreference mKillAppLongPressBack;
+    private SwitchPreference mDoubleTapSleepNarbar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,6 +34,13 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         int killAppLongPressBack = Settings.Secure.getInt(getContentResolver(),
                 KILL_APP_LONGPRESS_BACK, 0);
         mKillAppLongPressBack.setChecked(killAppLongPressBack != 0);
+
+        // Double tap 2 sleep on navi
+        mDoubleTapSleepNarbar = (SwitchPreference) findPreference(DOUBLE_TAP_SLEEP_NAVBAR);
+        mDoubleTapSleepNarbar.setOnPreferenceChangeListener(this);
+        int doubleTapSleepNarbar = Settings.Secure.getInt(getContentResolver(),
+                DOUBLE_TAP_SLEEP_NAVBAR, 0);
+        mDoubleTapSleepNarbar.setChecked(doubleTapSleepNarbar != 0);
     }
 
     @Override
@@ -39,6 +48,10 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         if (preference == mKillAppLongPressBack) {
             boolean value = (Boolean) objValue;
             Settings.Secure.putInt(getContentResolver(), KILL_APP_LONGPRESS_BACK,
+                    value ? 1 : 0);
+        } else if (preference == mDoubleTapSleepNarbar) {
+            boolean value = (Boolean) objValue;
+            Settings.Secure.putInt(getContentResolver(), DOUBLE_TAP_SLEEP_NAVBAR,
                     value ? 1 : 0);
         }
             return true;
